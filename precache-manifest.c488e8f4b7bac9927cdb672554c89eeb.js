3466db9fe022792723d3e145e2bbbc5ed649225e
Amay Kataria <amkatari@microsoft.com>
2020-06-04 11:18:31 -0500

    Updates

---
+++ precache-manifest.c488e8f4b7bac9927cdb672554c89eeb.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86391a14c38fa49c63d83322cc2ac1d1",
+    "url": "/InteractiveHolarchy/index.html"
+  },
+  {
+    "revision": "31306732f32d7463fa24",
+    "url": "/InteractiveHolarchy/static/css/main.1d25fd63.chunk.css"
+  },
+  {
+    "revision": "401017947fe8fac900d0",
+    "url": "/InteractiveHolarchy/static/js/2.ea2d5703.chunk.js"
+  },
+  {
+    "revision": "90526b3367ca000531e0465e56eee9ab",
+    "url": "/InteractiveHolarchy/static/js/2.ea2d5703.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31306732f32d7463fa24",
+    "url": "/InteractiveHolarchy/static/js/main.06444124.chunk.js"
+  },
+  {
+    "revision": "1ccb4cc60f5685a0642c",
+    "url": "/InteractiveHolarchy/static/js/runtime-main.e7d4c94f.js"
+  },
+  {
+    "revision": "ac45464b3fae4ecf677e7f5febd4d540",
+    "url": "/InteractiveHolarchy/static/media/p1.ac45464b.jpg"
+  },
+  {
+    "revision": "adcac74e98ae2bc0688e70389c056d8c",
+    "url": "/InteractiveHolarchy/static/media/p1.adcac74e.jpg"
+  },
+  {
+    "revision": "bfec8c722e0ca395e6ef9f04e3ec4d71",
+    "url": "/InteractiveHolarchy/static/media/p1.bfec8c72.jpg"
+  },
+  {
+    "revision": "6b84f0b24d308aaad4afad17156804cb",
+    "url": "/InteractiveHolarchy/static/media/p10.6b84f0b2.jpg"
+  },
+  {
+    "revision": "14ba14eecc12f9e79481dc0d69271ca1",
+    "url": "/InteractiveHolarchy/static/media/p11.14ba14ee.jpg"
+  },
+  {
+    "revision": "3b3f24af5c663b29431c689d25982b30",
+    "url": "/InteractiveHolarchy/static/media/p12.3b3f24af.jpg"
+  },
+  {
+    "revision": "e7e27e4d5bb3818232de1b519a032e8c",
+    "url": "/InteractiveHolarchy/static/media/p13.e7e27e4d.jpg"
+  },
+  {
+    "revision": "62dfc7d9ac1b98db5ceb49fcfaa083b5",
+    "url": "/InteractiveHolarchy/static/media/p14.62dfc7d9.jpg"
+  },
+  {
+    "revision": "996744a6c3d02295dbf4ef5c12dd0eb8",
+    "url": "/InteractiveHolarchy/static/media/p15.996744a6.jpg"
+  },
+  {
+    "revision": "7866f52e33bba7268a7e64e327e8ce2c",
+    "url": "/InteractiveHolarchy/static/media/p16.7866f52e.jpg"
+  },
+  {
+    "revision": "7396f0c58fe871de1f42ae6933f91aaa",
+    "url": "/InteractiveHolarchy/static/media/p17.7396f0c5.jpg"
+  },
+  {
+    "revision": "b97dc4a1f285ca37dc9355ac452c65dd",
+    "url": "/InteractiveHolarchy/static/media/p18.b97dc4a1.jpg"
+  },
+  {
+    "revision": "3a4ccd6fbc4ae3d2972a66eb965f41b6",
+    "url": "/InteractiveHolarchy/static/media/p19.3a4ccd6f.jpg"
+  },
+  {
+    "revision": "8286d1c81a95a7db8df3d2830c5f0237",
+    "url": "/InteractiveHolarchy/static/media/p2.8286d1c8.jpg"
+  },
+  {
+    "revision": "ace16c545039fe0d00eaf2b211a70213",
+    "url": "/InteractiveHolarchy/static/media/p2.ace16c54.jpg"
+  },
+  {
+    "revision": "dc8aca1a7d317d07d72fc7a97eec498f",
+    "url": "/InteractiveHolarchy/static/media/p2.dc8aca1a.jpg"
+  },
+  {
+    "revision": "d1de3fedf29973f378272e7b43c96012",
+    "url": "/InteractiveHolarchy/static/media/p20.d1de3fed.jpg"
+  },
+  {
+    "revision": "31d788aea6154c1fc15a4a8d4621cac8",
+    "url": "/InteractiveHolarchy/static/media/p3.31d788ae.jpg"
+  },
+  {
+    "revision": "57ec3787fc39b3a60725a6c86af2577a",
+    "url": "/InteractiveHolarchy/static/media/p3.57ec3787.jpg"
+  },
+  {
+    "revision": "6c3e509a94660928cfd8a110b6f2afef",
+    "url": "/InteractiveHolarchy/static/media/p3.6c3e509a.jpg"
+  },
+  {
+    "revision": "072ba1fe62f5b7f6209f6acce121c9ab",
+    "url": "/InteractiveHolarchy/static/media/p4.072ba1fe.jpg"
+  },
+  {
+    "revision": "d02c37e13089dda17d17749e483db5c0",
+    "url": "/InteractiveHolarchy/static/media/p4.d02c37e1.jpg"
+  },
+  {
+    "revision": "fc3e20c22921fe7c91797229d9174693",
+    "url": "/InteractiveHolarchy/static/media/p4.fc3e20c2.jpg"
+  },
+  {
+    "revision": "6b9cff4547541bd310da64c6ca60d761",
+    "url": "/InteractiveHolarchy/static/media/p5.6b9cff45.jpg"
+  },
+  {
+    "revision": "b60d00e3dfef13e25c6af14ca0236521",
+    "url": "/InteractiveHolarchy/static/media/p5.b60d00e3.jpg"
+  },
+  {
+    "revision": "dbf4d6e0a742c9ad667a97337f50cca0",
+    "url": "/InteractiveHolarchy/static/media/p5.dbf4d6e0.jpg"
+  },
+  {
+    "revision": "0a03bd49664ddbc8eb7ea52ebfbb7a52",
+    "url": "/InteractiveHolarchy/static/media/p6.0a03bd49.jpg"
+  },
+  {
+    "revision": "aed826a85728ae43d69a453247196aa9",
+    "url": "/InteractiveHolarchy/static/media/p6.aed826a8.jpg"
+  },
+  {
+    "revision": "cf3336faf5b56f7a2796865e503457d5",
+    "url": "/InteractiveHolarchy/static/media/p6.cf3336fa.jpg"
+  },
+  {
+    "revision": "57c1ffd1eb4b74ea40d187e428e8cc89",
+    "url": "/InteractiveHolarchy/static/media/p7.57c1ffd1.jpg"
+  },
+  {
+    "revision": "16f58648feb35d98535d5e88ebde687f",
+    "url": "/InteractiveHolarchy/static/media/p8.16f58648.jpg"
+  },
+  {
+    "revision": "300796bfeb282198f272c069002a29eb",
+    "url": "/InteractiveHolarchy/static/media/p9.300796bf.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.05e6378e64b0000e81c50ed321f19e82.js"
+  "/InteractiveHolarchy/precache-manifest.c488e8f4b7bac9927cdb672554c89eeb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/InteractiveHolarchy/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
